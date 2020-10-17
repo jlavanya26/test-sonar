7aaf9a2b4bc8221291e681a35d43b532e210cec3
Lavanya J <1245817@01HW743812.tcsgegdc.com>
2020-10-17 20:08:34 +0530

    removing unwanted files

--- src/main/java/hello/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        Message message = new Message();
-        System.out.println(message.sayHello());
-    }
-}
